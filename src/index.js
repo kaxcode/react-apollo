196b2b171f7e3d01d051678a3afd28deb4431df5
Kenia Valladarez <keniavalladarez@gmail.com>
2018-04-15 23:37:12 -0400

    Configure Appollo Client.

--- src/index.js
+++ src/index.js
@@ -4,5 +4,26 @@ import './styles/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Add Apollo Packages needed
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+// Create HTTPLink- Connecting ApolloClient to GraphQL API
+const httpLink = new HttpLink({ uri: 'http://localhost:4000' });
+
+// Instantiate the ApolloClient and create new InMemoryCache
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
+// Redering the root component. App is wrapped with HO component ApolloProvider
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
