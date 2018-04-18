020032b25c32a99be564ce82af090db1453d75a8
Kenia Valladarez <keniavalladarez@gmail.com>
2018-04-17 22:02:37 -0400

    Removes commented code

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import LinkList from './LinkList';
 import '../styles/App.css';
-// import CreateLink from './CreateLink';
 
 class App extends Component {
   render() {
     return <LinkList />;
-    // return <CreateLink />;
   }
 }
 
